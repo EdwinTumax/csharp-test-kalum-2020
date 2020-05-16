11e7bc4cd9d380570cb40658302a6cd5a38a7f61
Edwin Tumax <edwintumax@gmail.com>
2020-05-16 08:52:23 -0600

    Tarea Creación de modelo de datos de Login

--- DataContext/KalumDbContext.cs
+++ DataContext/KalumDbContext.cs
@@ -16,8 +16,11 @@ namespace Kalum2020v1.DataContext
         public DbSet<Salon> Salones {get;set;}
         public DbSet<Instructor> Instructores {get;set;}   
         public DbSet<Clase> Clases {get;set;}        
-
         public DbSet<Religion> Religiones {get;set;}
+
+        public DbSet<Rol> RolesApp {get;set;}
+        public DbSet<Usuario> UsuariosApp {get;set;}
+        public DbSet<UsuarioRol> UsuariosRoles {get;set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
@@ -26,10 +29,17 @@ namespace Kalum2020v1.DataContext
             .Build();
             optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));            
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UsuarioRol>()
+                .HasKey(x => new { x.UsuarioId, x.RoleId });
+        }
         public KalumDbContext()
         {
 
         }
+
+        
     }
 
     
